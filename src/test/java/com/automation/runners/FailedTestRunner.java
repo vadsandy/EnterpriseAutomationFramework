1e24803e8e2c3652e317b0ccace3503e42472ee5
vadsandy <sandeep.dct@gmail.com>
2026-05-24 17:47:45 +0530

    Added rerun execution passes

--- src/test/java/com/automation/runners/FailedTestRunner.java
+++ src/test/java/com/automation/runners/FailedTestRunner.java
@@ -5,18 +5,18 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        tags = "@Regression",
-        features = "src/test/resources/features",
+        // NEW CHANGE: Points directly to the rerun text file instead of the features folder
+        features = "@target/failed_rerun.txt",
         glue = {"com.automation.stepdefs"},
         plugin = {
                 "pretty",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", // NEW CHANGE: Allure plugin
-                "rerun:target/failed_rerun.txt" // NEW CHANGE: Records failed scenarios here
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "rerun:target/failed_rerun.txt" // Overwrites the file if retries fail again
         },
         monochrome = true,
         publish = false
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
 
     @Override
     @DataProvider(parallel = true)
